4aeb6fbe8e230fc62cdec58f682ef7b44851b72a
Apiryy <samuknou@gmail.com>
2021-11-14 16:48:33 +0100

    Mon premier commit
    Premier commit

---
+++ Assets/Scripts/Health/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(1f); // Prendre la couleur au max donc 1
+    }
+    
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue); // Prendre la couleur ou est la bar de vie
+    }
+}
